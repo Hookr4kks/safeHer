8f4dd8fe5aebd9746bb05d3189ed8c75672afd70
zkw <112327590+fasxzt@users.noreply.github.com>
2026-08-02 01:56:33 -0300

    Add files via upload

---
+++ safeHer/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAuGD7oocCzuiyicZxpYtnu80vvTeXDA6U",
+  authDomain: "safeher-43cd3.firebaseapp.com",
+  projectId: "safeher-43cd3",
+  storageBucket: "safeher-43cd3.firebasestorage.app",
+  messagingSenderId: "1065994317910",
+  appId: "1:1065994317910:web:f53a29d4b9fe0a70914bf7"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+googleProvider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export { auth, db, googleProvider };
